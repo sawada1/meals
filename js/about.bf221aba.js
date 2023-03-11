3e05dd53e10954d372b1c097f36eb1c92ef47a97
sawada1 <khaledsawada1@gmail.com>
2023-03-12 01:48:15 +0200

    adding dist

---
+++ js/about.bf221aba.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmeals"]=self["webpackChunkmeals"]||[]).push([[443],{9775:function(n,a,e){e.r(a),e.d(a,{default:function(){return b}});var s=e(3396);const t={class:"about"},u=(0,s._)("h1",null,"This is an about page",-1),r=[u];function c(n,a,e,u,c,o){return(0,s.wg)(),(0,s.iD)("div",t,r)}var o=e(7e3),i={components:{navbar:o.Z}},l=e(89);const f=(0,l.Z)(i,[["render",c]]);var b=f}}]);
+//# sourceMappingURL=about.bf221aba.js.map
